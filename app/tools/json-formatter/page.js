2fd6493442cb7ae372cd9e77c82f9bbbc1980d83
aditehh <aditya.aryal888@gmail.com>
2025-10-07 23:54:53 +0545

    this

--- app/tools/json-formatter/page.js
+++ app/tools/json-formatter/page.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
 const page = () => {
-  return (
-    <>
+    return (
+        <div className="flex min-h-[85vh] max-w-4xl py-20 mx-auto px-6 text-black">
+                <div>
+                    hh
+                </div>
 
 
 
 
 
-    
-    </>
-  )
+        </div>
+    )
 }
 
 export default page
